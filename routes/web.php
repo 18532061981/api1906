1eadc3228ff68a03cc9e6d85c8d1776b2f04bd49
wzd <3051037702@qq.com>
2020-02-17 21:39:24 +0800

    统计浏览量

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::prefix('/wx')->group(function () {
+    Route::any('access_token','Test\TestController@access_token');
+    Route::any('curl1','Test\TestController@curl1');
+    Route::any('curl2','Test\TestController@curl2');
+    Route::any('guzzle','Test\TestController@guzzle');
+});
+
+
+Route::prefix('/goods')->group(function () {
+    Route::any('detail','Goods\GoodsController@detail');
+    Route::any('getUrl','Goods\GoodsController@getUrl');
+    Route::any('redisStr1','Goods\GoodsController@redisStr1');
+});
